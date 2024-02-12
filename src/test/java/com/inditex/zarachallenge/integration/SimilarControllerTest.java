9dc65ee7d307d68cae1ba51651ce3fb30518c11b
Darwin <darwin.martinez@comafi.com.ar>
2024-02-11 22:13:37 -0300

    correccion de casos de prueba

--- src/test/java/com/inditex/zarachallenge/integration/SimilarControllerTest.java
+++ src/test/java/com/inditex/zarachallenge/integration/SimilarControllerTest.java
@@ -2,15 +2,22 @@ package com.inditex.zarachallenge.integration;
 
 import com.inditex.zarachallenge.application.controller.SimilarController;
 import com.inditex.zarachallenge.application.dto.response.ProductDetailResponse;
+import com.inditex.zarachallenge.application.mapper.ProductMapper;
 import com.inditex.zarachallenge.application.service.ProductService;
 import com.inditex.zarachallenge.domain.model.ProductEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.inditex.zarachallenge.application.controller.ProductHandlerException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,19 +26,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SimilarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Mock
     private ProductService productService;
+
+    @Mock
+    private ProductMapper productMapper;
 
     @InjectMocks
     private SimilarController similarController;
 
     @BeforeEach
     void setUp() {
-        productService = mock(ProductService.class);
-        similarController = new SimilarController();
-
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
@@ -41,6 +53,7 @@ public class SimilarControllerTest {
         List<ProductEntity> productList = new ArrayList<>();
         productList.add(new ProductEntity());
         when(productService.findSimilarProducts(productId)).thenReturn(productList);
+        when(productMapper.mapProductEntitiesToProductDetailResponse(anyList())).thenReturn(new ProductDetailResponse());
 
         ResponseEntity<ProductDetailResponse> responseEntity = similarController.getSimilarProducts(productId);
 
